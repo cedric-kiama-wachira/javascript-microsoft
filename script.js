049ca42b072cd9a2c6bebb4f810ebe5c4529be54
Cedric K. Wachira <cedric.kiama@gmail.com>
2024-02-16 20:21:10 +0400

    more practice

---
+++ script.js
@@ -0,0 +1,28 @@
+// let width = prompt("Volume of the box, enter width", 0);
+// let height = prompt("Volume of the box, enter height", 0);
+// let length = prompt("Volume of the box, enter length", 0);
+// let volume = width * height * length;
+// alert(`Calculated box volume is ${volume}`);
+
+// let names = prompt("Please  enter your names here", "John Doe");
+// let phone = prompt("Mobile Number", +971);
+// let email = prompt("enter your email address", email@example.com);
+// let volume = width * height * length;
+// alert(`Calculated box volume is ${volume}`);
+
+// let n = 2;
+// n = n*n*n;
+// console.log(n);
+// n**=3
+// console.log(n)
+// console.log(20||5);
+// console.log(false||"false")
+
+// let str = "12";
+// str = +str;
+// console.log(str);
+
+// let n = 2 * 3 ** 3 - 1;
+// console.log(n);
+
+let test = prompt("Hello","World");
