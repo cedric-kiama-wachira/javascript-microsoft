204f1bfe4ad790acfee166d1ca29b5371424edfb
Cedric K. Wachira <cedric.kiama@gmail.com>
2024-02-21 20:25:21 +0400

    More practice

--- script.js
+++ script.js
@@ -1,81 +1,81 @@
-// // 1.Define a simple function
-// function helloCedric(){
-//     console.log("Invoking the function using the function name helloCedric()")
-// }
-// console.log(typeof helloCedric);
+// 1.Define a simple function
+function helloCedric(){
+    console.log("Invoking the function using the function name helloCedric()")
+}
+console.log(typeof helloCedric);
 
-// // 2. Invoke the function
-// helloCedric();
+// 2. Invoke the function
+helloCedric();
 
-// // 3. Rules for naming a function
-// /**
-//  *  _
-//  * A-Z | a -z
-//  * $
-//  * numbers
-//  */
+// 3. Rules for naming a function
+/**
+ *  _
+ * A-Z | a -z
+ * $
+ * numbers
+ */
 
-// let a = 100, b = 200, c = 300;
+let a = 100, b = 200, c = 300;
 
-// function test(a) {
-//     let b = 10;
-//     console.log(a); // parameter a
-//     console.log(b); // local variable b
-//     console.log(c); // global variable c
-// }
+function tester(a) {
+    let b = 10;
+    console.log(a); // parameter a
+    console.log(b); // local variable b
+    console.log(c); // global variable c
+}
 
-// test(1);        // -> 1
-//                 // -> 10
-//                 // -> 300
+tester(1);        // -> 1
+                // -> 10
+                // -> 300
 
-// console.log(a); // -> 100
-// console.log(b); // -> 200
-// console.log(c); // -> 300
+console.log(a); // -> 100
+console.log(b); // -> 200
+console.log(c); // -> 300
 
-
-
-// let x = 10;
-
-// function test() {
-//     let x = 20;
-// }
-
-// test();
-// console.log(x);
-
-// let test = "testing";
-
-// let show = function(){
-//     console.log(test);
-
-// }
-
-// setTimeout(show, 2000);
-
-
-// let x = 10;
-// let y = 20;
-
-// function test(y) {
-//     console.log(y)
-// }
-// test(x);
-
-
-// function test(counter){
-//     console.log("test");
-//     if (counter > 0)
-//     test(--counter);
-// }
-
-// test(3);
 
 
 let x = 10;
 
-function test(){
+function testin() {
     let x = 20;
-    console.log(x)
+}
+
+testin();
+console.log(x);
+
+let testers = "testing";
+
+let show = function(){
+    console.log(testers);
+
+}
+
+setTimeout(show, 2000);
+
+
+let sx = 10;
+let y = 20;
+
+function testy(y) {
+    console.log(y)
+}
+testy(sx);
+
+
+function testa(counter){
+    console.log("test");
+    if (counter > 0)
+    testa(--counter);
+}
+
+testa(3);
+
+
+let bx = 10;
+
+function test(){
+    let bx = 20;
+    console.log(bx)
 }
 
 test();
